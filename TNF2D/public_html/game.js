9a82840b704f4daec50d4f6d3f63130c532b4bec
Jami Karvanen <jami@karvanen.fi>
2013-01-26 00:40:24 +0200

    Layout-progress and some includes

--- TNF2D/public_html/game.js
+++ TNF2D/public_html/game.js
@@ -6,6 +6,33 @@
  * To change this template use File | Settings | File Templates.
  */
 
+// RequestAnimationFrame polyfill
+(function() {
+    var lastTime=0;
+    var vendors=['ms', 'moz', 'webkit', 'o'];
+    for (var x=0; x<vendors.length && !window.requestAnimationFrame; x++) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[x]+'CancelAnimationFrame'] ||
+                window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime.timeToCall); },
+                timeToCall);
+            lastTime = currTime+timeToCall;
+            return id;
+        };
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
 $(document).ready(function() {
     game.init();
 });
@@ -18,13 +45,29 @@ var game = {
     init: function() {
         // TODO init game
         game.context = $('#gamecanvas')[0].getContext('2d');
+
+        // Näytetään päämenu
+        $('.gamelayer').hide();
+        $('#startscreen').show();
     },
     start: function() {
 
     }
 }
 
+var levels = {
+   definitions: [{
+
+   }]
+}
+
 var entities = {
+    definitions: [{
+        name: "blank",
+        type: "blank",
+        width:0,
+        height:0
+    }],
     create: function() {
 
     },
@@ -32,3 +75,64 @@ var entities = {
 
     }
 }
+
+var loader = {
+
+    loaded: true,
+    loadedCount: 0,
+    totalCount: 0,
+
+    init: function() {
+        var mp3Support, oggSupport;
+        var audio = document.createElement('audio');
+        if (audio.canPlayType) {
+            mp3Support = "" !=audio.canPlayType('audio/meg');
+            oggSupport = "" !=audio.canPlayType('audio/ogg; codecs="vorbis"');
+        } else {
+            mp3Support = false;
+            oggSupport = false;
+        }
+
+        loader.soundFileExtn = oggSupport?".ogg":mp3Support?".mp3":undefined;
+    },
+
+    loadImage: function(url) {
+        this.totalCount++;
+        this.loaded = false;
+        $('#loadingscreen').show();
+        var image = new Image();
+        image.src=url;
+        image.onload=loader.itemLoaded;
+        return image;
+    },
+
+    soundFileExtn:".ogg",
+
+    loadSound: function(url) {
+        this.totalCount++;
+        this.loaded = false;
+        $('#loadingscreen').show();
+        var audio = new Audio();
+        audio.src=url+loader.soundFileExtn;
+        audio.addEventListener("canplaythrough", loader.itemLoaded, false);
+        return audio;
+    },
+
+    itemLoaded: function() {
+        loader.loadedCount++;
+        $('#loadingmessage').html('Loaded ' +loader.loadedCount+' of '+loader.totalCount);
+        if (loader.loadedCount === loader.totalCount) {
+            // Loader on valmis
+            loader.loaded = true;
+            // Piilotetaan latausviesti
+            $('#loadingscreen').hide();
+            // Kutsutaan loader.onloadia, jos sellainen löytyy
+            if (loader.onload) {
+                loader.onload();
+                loader.onload = undefined;
+            }
+
+        }
+    }
+}
+

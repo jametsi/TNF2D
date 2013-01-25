2b35cbc71b85d4878f63c5884aeb229f843e8b6e
Jami Karvanen <jami@karvanen.fi>
2013-01-25 23:38:34 +0200

    First commit!

--- TNF2D/public_html/game.js
+++ TNF2D/public_html/game.js
@@ -5,6 +5,30 @@
  * Time: 22:52
  * To change this template use File | Settings | File Templates.
  */
+
+$(document).ready(function() {
+    game.init();
+});
+
 var game = {
 
+    context: {},
+    hero: {},
+
+    init: function() {
+        // TODO init game
+        game.context = $('#gamecanvas')[0].getContext('2d');
+    },
+    start: function() {
+
+    }
+}
+
+var entities = {
+    create: function() {
+
+    },
+    draw: function(entity) {
+
+    }
 }

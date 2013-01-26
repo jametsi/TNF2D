9ffc0d64edaa63a68bdd4b0e4d0b1132762fa823
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-01-26 23:10:32 +0200

    hero fixed or so

--- TNF2D/public_html/hero.js
+++ TNF2D/public_html/hero.js
@@ -4,21 +4,23 @@ function Hero(startVector) {
     this.walkingSpeed = 1.3;
     this.width = 100;
     this.height = 100;
-    this.angle = 180;
+    this.angle = 0;
     this.sprite = loader.loadImage("img/herosheet.png");
     console.log(this.sprite);
 
 }
 
 Hero.prototype.update = function() {
-   this.turn();
-  //  this.move();
+    this.turn();
+    if(keyhandler.up) {
+        this.move();
+    }
 }
 
 Hero.prototype.turn = function() {
     var dx = game.painter.camera.x - keyhandler.cursorX;
     var dy = game.painter.camera.y - keyhandler.cursorY;
-    var newAngle= Math.atan2(dy, dx)*(180/Math.PI);
+    var newAngle = -(Math.atan2(dy, dx)*(180/Math.PI));
     console.log(newAngle);
 
     if(newAngle > this.angle) {

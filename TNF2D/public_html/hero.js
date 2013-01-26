15f812aad27eca3eb4a9a402836567bb6b665324
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-01-27 00:51:19 +0200

    changes

--- TNF2D/public_html/hero.js
+++ TNF2D/public_html/hero.js
@@ -4,8 +4,12 @@ function Hero(startVector) {
     this.walkingSpeed = 1.3;
     this.width = 100;
     this.height = 100;
+    this.lastAnimFrame = 0;
     this.angle = 0;
-    this.sprite = loader.loadImage("img/herosheet.png");
+    this.sprite = loader.loadImage("img/hahmp_spritesheet_final.png");
+    this.spritewidth = 100;
+    this.spriteheight = 100;
+    this.walking = false;
     console.log(this.sprite);
 
 }
@@ -13,15 +17,28 @@ function Hero(startVector) {
 Hero.prototype.update = function() {
     this.turn();
     if(keyhandler.up) {
+        this.walking = true;
         this.move();
+    } else {
+        this.walking = false;
     }
+}
+
+Hero.prototype.changeFrame = function() {
+    var max = 3;
+    if(this.walking) {
+        this.lastAnimFrame++;
+    }
+    if(this.lastAnimFrame == max) {
+        this.lastAnimFrame = 0;
+    }
+
 }
 
 Hero.prototype.turn = function() {
     var dx = keyhandler.cursorX - game.canvas.width/2;
     var dy = keyhandler.cursorY - game.canvas.height/2;
     var newAngle = -(Math.atan2(dy, dx)*(180/Math.PI));
-    console.log(newAngle);
 
     if(newAngle > this.angle % 360) {
         this.turnRight();

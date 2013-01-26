b5c072e3249c87492e80c33188d8178345041739
Jami Karvanen <jami@karvanen.fi>
2013-01-27 00:56:01 +0200

    collision detection

--- TNF2D/public_html/hero.js
+++ TNF2D/public_html/hero.js
@@ -4,41 +4,24 @@ function Hero(startVector) {
     this.walkingSpeed = 1.3;
     this.width = 100;
     this.height = 100;
-    this.lastAnimFrame = 0;
     this.angle = 0;
-    this.sprite = loader.loadImage("img/hahmp_spritesheet_final.png");
-    this.spritewidth = 100;
-    this.spriteheight = 100;
-    this.walking = false;
-    console.log(this.sprite);
+    this.sprite = loader.loadImage("img/herosheet.png");
+    //console.log(this.sprite);
 
 }
 
 Hero.prototype.update = function() {
     this.turn();
     if(keyhandler.up) {
-        this.walking = true;
         this.move();
-    } else {
-        this.walking = false;
     }
-}
-
-Hero.prototype.changeFrame = function() {
-    var max = 3;
-    if(this.walking) {
-        this.lastAnimFrame++;
-    }
-    if(this.lastAnimFrame == max) {
-        this.lastAnimFrame = 0;
-    }
-
 }
 
 Hero.prototype.turn = function() {
     var dx = keyhandler.cursorX - game.canvas.width/2;
     var dy = keyhandler.cursorY - game.canvas.height/2;
     var newAngle = -(Math.atan2(dy, dx)*(180/Math.PI));
+    //console.log(newAngle);
 
     if(newAngle > this.angle % 360) {
         this.turnRight();
@@ -59,6 +42,35 @@ Hero.prototype.turnLeft = function() {
 
 
 Hero.prototype.move = function() {
-    this.position.x += Math.sin(this.angle * (Math.PI / 180)+ Math.PI/2) * this.walkingSpeed;
-    this.position.y += Math.cos(this.angle * (Math.PI / 180)+ Math.PI/2) * this.walkingSpeed;
+    var x = Math.floor(this.position.x/400);
+    var y = Math.floor(this.position.y/400);
+
+    var positionX = this.position.x/400-(Math.floor(this.position.x/400));
+    var positionY = this.position.y/400-(Math.floor(this.position.y/400));
+
+    var xMovement = Math.sin(this.angle * (Math.PI / 180)+ Math.PI/2) * this.walkingSpeed;
+    var yMovement = Math.cos(this.angle * (Math.PI / 180)+ Math.PI/2) * this.walkingSpeed;
+
+    if (xMovement > 0) {
+        if(dungeon.map[x][y].RIGHTWALL && positionX > .64) {
+            xMovement = 0;
+        }
+    }
+    if (xMovement < 0) {
+        if(dungeon.map[x][y].LEFTWALL && positionX < .36) {
+            xMovement = 0;
+        }
+    }
+    if (yMovement > 0) {
+        if(dungeon.map[x][y].TOPWALL && positionY > .64) {
+            yMovement = 0;
+        }
+    }
+    if (yMovement < 0) {
+        if(dungeon.map[x][y].LEFTWALL && positionY < .36) {
+            yMovement = 0;
+        }
+    }
+    this.position.x += xMovement;
+    this.position.y += yMovement;
 }

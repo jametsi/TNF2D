f701decc25fc84cbc6327b7d0d63f49be73d4db8
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-01-27 06:17:43 +0200

    splatter-effects

--- TNF2D/public_html/splatter.js
+++ TNF2D/public_html/splatter.js
@@ -5,3 +5,32 @@
  * Time: 5:27 AM
  * To change this template use File | Settings | File Templates.
  */
+
+function Splatter(x, y) {
+    this.x = x;
+    this.y = y;
+    this.position = new Vector(x,y);
+    this.lifetime = 360;
+    this.counter = 0;
+    this.size = 5 + Math.random()*10 ;
+    this.alive = true;
+    this.color = 'rgba(255,0,0,';
+}
+
+Splatter.prototype.update = function() {
+    if(this.alive) {
+        this.counter++;
+    }
+    if(this.counter >= this.lifetime) {
+        this.alive = false;
+    }
+}
+
+Splatter.prototype.getColor = function() {
+    var alphavalue = 1.0 - Math.round((this.counter / this.lifetime)*10)/10;
+    return this.color + "" + alphavalue + ")";
+}
+
+Splatter.prototype.getSize = function() {
+    return this.size *( 1.0 + Math.round((this.counter / this.lifetime)*10)/10);
+}

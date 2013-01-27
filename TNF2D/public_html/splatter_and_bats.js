7ce0823ae43e58400b46c8a87cb934e9a0752ace
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-01-27 12:57:16 +0200

    splatter-effects

--- TNF2D/public_html/splatter_and_bats.js
+++ TNF2D/public_html/splatter_and_bats.js
@@ -12,9 +12,11 @@ function Splatter(x, y) {
     this.position = new Vector(x,y);
     this.lifetime = 360;
     this.counter = 0;
+    this.lastAnimFrame = 0;
+    this.spritewidth = 50;
     this.size = 5 + Math.random()*10 ;
     this.alive = true;
-    this.color = 'rgba(255,0,0,';
+    this.color = 'rgba(255,0,0,)';
 }
 
 Splatter.prototype.update = function() {
@@ -33,4 +35,28 @@ Splatter.prototype.getColor = function() {
 
 Splatter.prototype.getSize = function() {
     return this.size *( 1.0 + Math.round((this.counter / this.lifetime)*10)/10);
+}
+
+function Bat(x, y) {
+    this.x = x;
+    this.y = y;
+    this.position = new Vector(x,y);
+    this.lifetime = 400;
+    this.counter = 0;
+    this.size; 50;
+    this.alive = true;
+}
+
+Bat.prototype.update = function() {
+
+    if(this.alive && this.counter < this.lifetime) {
+        this.counter++;
+    } else {
+        this.alive = false;
+    }
+    if(this.alive) {
+        if(this.counter > 0 && this.counter % 120 ) {
+            this.lastAnimFrame++;
+        }
+    }
 }

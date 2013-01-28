a31961b36b482ae7d080dd388352973148801553
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-01-28 19:21:06 +0200

    fixed overlay canvas related things

--- TNF2D/public_html/graphics.js
+++ TNF2D/public_html/graphics.js
@@ -51,9 +51,9 @@ Painter.prototype.drawFlashLight = function() {
 
     var height = this.flashLightImage.height;
     var width = this.flashLightImage.width;
-
+    game.overlay.clearRect(0,0,game.canvas.width,game.canvas.height);
     game.overlay.globalCompositeOperation = "lighter";
-    //  game.overlay.globalAlpha = 0.5;
+    game.overlay.globalAlpha = 0.5;
     game.overlay.translate(translatepaskex, translatepaskey);
     game.overlay.rotate(-game.hero.angle * Math.PI / 180 + Math.PI/2);
     game.overlay.drawImage(this.flashLightImage, -100, -220, width, height);
